1a73b904527160736e41559e1a8d167c34adc743
Hannah Drury <hannah_drury@yahoo.com>
2020-03-19 21:53:49 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,9 @@
+require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+  "ssssssssss" + string
   else
   string
   end
 end
+#binding.pry
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -12,7 +14,7 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  crew.collect {|crew_member| "Hello #{crew_member}."}
 end
 
 def engage
